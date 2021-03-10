03b5b42e9301a9e9c4a83a2171bcc289e7fb1b7a
Shobhit Vashistha <shobhit.vashistha@tarento.com>
2021-03-11 04:10:30 +0530

    notes, formatting

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 PWD=`pwd`
 
-sudo rm -rf $PWD/mock-kafka-data/.data
-
 # stop and remove previous container
 sudo docker stop sb-dp-test && sudo docker rm sb-dp-test
 # build the image
-sudo docker build -t sb-dp-test-image:1.0 .
+sudo docker build -t sb-dp-test-image:1.1 src/.
 # run container
-sudo docker run -d --name=sb-dp-test -v $PWD/mock-kafka-data:/mock-kafka-data sb-dp-test-image:1.0
+sudo docker run -d --name=sb-dp-test sb-dp-test-image:1.1
 # open logs
 sudo docker logs -f sb-dp-test
