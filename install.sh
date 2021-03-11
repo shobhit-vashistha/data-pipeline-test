7a1c5cc6258637933d13739429ffa2ad3df79bf3
Shobhit Vashistha <shobhit.vashistha@tarento.com>
2021-03-11 18:10:53 +0530

    notes, formatting

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ PWD=`pwd`
 # stop and remove previous container
 sudo docker stop sb-dp-test && sudo docker rm sb-dp-test
 # build the image
-sudo docker build -t sb-dp-test-image:1.1 src/.
+sudo docker build -t sb-dp-test-image:1.1 ${PWD}/src/.
 # run container
 sudo docker run -p "8000:8000" -d --name=sb-dp-test sb-dp-test-image:1.1
 # open logs
