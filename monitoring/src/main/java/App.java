e7d7ff5d9eded7af2ca2547a0ee9528dd43480f9
Shobhit Vashistha <shobhit.vashistha@tarento.com>
2021-03-17 22:10:02 +0530

    jar

--- monitoring/src/main/java/App.java
+++ monitoring/src/main/java/App.java
@@ -2,7 +2,7 @@ import socket.Server;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int port = 6666;
+        int port = 6660;
         if (args.length > 0) {
             port = Integer.parseInt(args[0]);
         }
--- monitoring/src/main/java/kafka/SingleTopicConsumer.java
+++ monitoring/src/main/java/kafka/SingleTopicConsumer.java
@@ -90,7 +90,7 @@ public class SingleTopicConsumer implements Closeable, AutoCloseable {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, IKafkaConstants.KAFKA_BROKERS);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, IKafkaConstants.GROUP_ID_CONFIG);
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, IKafkaConstants.MAX_POLL_RECORDS);
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
