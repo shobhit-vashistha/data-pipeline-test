f4acd2e7ee53dc3c6dbd00e913d1278bbd30e432
Shobhit Vashistha <shobhit.vashistha@tarento.com>
2021-03-17 21:56:29 +0530

    jar

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -5,6 +5,6 @@ sudo docker stop sb-dp-test-server && sudo docker rm sb-dp-test-server
 # build the image
 sudo docker build -t sb-dp-test-server-image:1.1 .
 # run container
-sudo docker run -p 6666:6666 -d --name=sb-dp-test sb-dp-test-server-image:1.1
+sudo docker run -p 6666:6666 -d --name=sb-dp-test-server sb-dp-test-server-image:1.1
 # open logs
 sudo docker logs -f sb-dp-test-server
